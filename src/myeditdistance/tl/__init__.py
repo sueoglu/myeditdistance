5daaccd21a63d6bd4c710db6ad5430ec9bcf607b
oykusuoglu <sueoglu@cip.ifi.lmu.de>
2025-06-13 14:46:17 +0200

    import fixed in init tl

--- src/myeditdistance/tl/__init__.py
+++ src/myeditdistance/tl/__init__.py
@@ -1,1 +1,2 @@
+from .basic import basic_tool
 from .calculation import edit_distance
--- src/myeditdistance/tl/edit_dist_api.py
+++ src/myeditdistance/tl/edit_dist_api.py
@@ -1,3 +1,4 @@
 import myeditdistance as ed
 
 distance = ed.edit_distance("kitten", "sitting")
+print(distance)
