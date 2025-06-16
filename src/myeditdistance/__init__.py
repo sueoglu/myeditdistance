e51c85febcd44ab6ce10f1ca09d270f5de5f1ca5
Eljas Roellin <65244425+eroell@users.noreply.github.com>
2025-06-16 11:28:29 +0200

    Update __init__.py

--- src/myeditdistance/__init__.py
+++ src/myeditdistance/__init__.py
@@ -2,10 +2,7 @@ from importlib.metadata import version
 
 from . import pl, pp, tl
 
-__all__ = ["pl", "pp", "tl"]
+__all__ = ["pl", "pp", "tl", "edit_distance"]
 
 __version__ = version("firsttask")
 
-
-def edit_distance(string1, string2):
-    return tl.edit_distance(string1, string2)
